7bd990802cdcfccc4c2963b84295a634799b007c
fainz777 <fainz777@gmail.com>
2019-09-29 23:00:03 +0300

    expression-calculator/feature: solution [in progress]

--- src/index.js
+++ src/index.js
@@ -3,10 +3,65 @@ function eval() {
     return;
 }
 
-function expressionCalculator(expr) {
-    // write your solution here
+const exprPattern = /(\d+\.?\d*?)\s*(\+|-|\/|\*)\s*(\d+)/;
+const zeroDivisionPattern = /\/\s*0/;
+
+const calculator = {
+	'+': function(a, b) {
+		return a + b;
+	},
+	'-': function(a, b) {
+		return a - b;
+	},
+	'*': function(a, b) {
+		return a * b;
+	},
+	'/': function(a, b) {
+		return a / b;
+	}
 }
 
-module.exports = {
-    expressionCalculator
+function getExpression(expr) {
+	return expr.match(exprPattern);
 }
+
+function calculateExpression(a, b, operator) {
+	if (!calculator[operator]) {
+		throw Error('Error');
+	}
+
+	return calculator[operator](a, b);
+}
+
+function expressionCalculator(expr) {
+	const isZeroDivision = expr.match(zeroDivisionPattern)
+
+	if (isZeroDivision) {
+		throw new TypeError('TypeError: Division by zero.');
+	}
+
+    const exp = getExpression(expr);
+
+    if (!exp) {
+    	return parseFloat(expr);
+    }
+
+    if (!calculator[exp[2]]) return;
+
+
+
+    let result = calculator[exp[2]](+exp[1], +exp[3]);
+    
+    expr = expr.replace(exprPattern, result);
+    
+    console.log(expr)
+
+    return expressionCalculator(expr);
+}
+
+const expr = " 59 - 13 + (  25 * 22 / (  47 / 38 * (  64 / 93 - 91 + 72  ) * 66  ) + 43 - 5  ) * 39 / 55 "
+console.log('72.6846 = ', expressionCalculator(expr))
+
+/*module.exports = {
+    expressionCalculator
+}*/
